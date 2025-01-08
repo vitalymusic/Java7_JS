b4f3799704e6611820d5768c95601375c7ac96dc
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-01-08 19:58:47 +0200

    Mainigie

--- main.js
+++ main.js
@@ -54,3 +54,120 @@ let attels = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_lo
 // maja.atveries();
 
 console.log(maja);
+
+
+
+
+// Operātori
+
+// +,-,*,/,%,++,--,+=,-=,*=,/=,**
+
+// Salīdzināšanas operātori
+// ==,!=,>,<,>=,<=,===,!==
+
+// Loģiskie operātori
+// &&,|| 
+
+
+let result = 23*34;
+
+let sk1 = 34.45,sk2 = 56.78;
+result = sk1+sk2;
+//result = result*10;
+result *=10;
+result++;
+++result;
+
++result; //unārais pluss
+
+
+result = 10.445345345.toFixed(2);
+
+
+// result = +prompt("Ievadi pirmo skaitli") * +prompt("Ievadi otru skaitli");
+result = Math.round(sk1);
+
+result = Math.trunc(Math.random()*100);
+
+result = Math.PI.toFixed(2);
+
+document.write("<h1>");
+document.write(result);
+document.write("</h1>");
+
+
+// Teksta virkņu metodes
+
+
+// Konkatenācija 
+let diena = "Trešdiena";
+
+
+document.write("Šodien ir: " + diena + "!");
+
+let html = "<a href='"+diena+".html'>"+ diena + "</a>";
+ html = `<a href="${diena}.html">${diena}</a>`;
+
+let img = `<img src="${attels}" width="200">`;
+
+let dienas = ["Pirmdiena","Otrdiena","Trešdiena"];
+
+let table = "<table>";
+table += "<tr>";
+table += "<td>dati</td>";
+table += "</tr>";
+table += "</table>";
+
+
+table = `
+<table border="1" width="100%">
+    <tr>
+        <td>${dienas[0]}</td>
+        <td>${dienas[1]}</td>
+        <td>${dienas[2]}</td>
+    </tr>    
+</table>    
+`;
+
+
+
+let teksts = `The Math.PI static data property represents the ratio of the circumference of a circle to its diameter, approximately 3.14159.  Math.PI`;
+
+
+
+let textResult = teksts.charAt(1);
+ textResult = teksts[2];
+ textResult = teksts.slice(5,-5);
+ textResult = teksts.slice(0,-5);
+ textResult = teksts.split("");
+ textResult = teksts.split("i");
+ textResult = teksts.toUpperCase();
+ textResult = teksts.trim();
+ textResult = teksts.replace("Math.PI","Matemātiskais PI");   
+ textResult = teksts.replaceAll(" ","::::");   
+    // teksts.match();
+
+
+textResult = teksts.length;
+
+ 
+
+
+
+
+
+
+document.write("<br>"+textResult);
+console.log(textResult);
+
+// document.write(html);
+// document.write(img);
+// document.write(table);
+
+
+
+
+
+
+
+
