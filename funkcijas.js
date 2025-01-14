758e189d55707df9edfe00086457aada4e37a22c
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-01-14 19:27:57 +0200

    sask

--- funkcijas.js
+++ funkcijas.js
@@ -1,4 +1,8 @@
+let skaitlis = 10;
+
 function saskaitisana(sk1,sk2){
+        // let skaitlis="6";
+        console.log(skaitlis);
     // darbība ko funkcijas izpilda
     return sk1+sk2;
 }
@@ -6,6 +10,7 @@ function saskaitisana(sk1,sk2){
 // saskaitisana(123123,23423432);
 
 function izvade(rezultats){
+        let skaitlis = 34;
     document.write(`
         <div style="border:2px solid red;padding:10px;font-size:25pt">
             Rezultāts ir: ${rezultats}!
@@ -14,3 +19,14 @@ function izvade(rezultats){
 }
 
 izvade(saskaitisana(123123,234234));
+
+
+
+
+// saskaitit()
+
+
+function saskaitit(){
+    
+
+}
